de5bd9bf53823bb4a4c1e987b27c8460c7153c39
Cliff Tanaka <tanaka.cliff@gmail.com>
2017-03-24 01:50:56 +0900

    add map button and what not

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -9,14 +9,46 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
         mapView.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 6 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
+        
+    }
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            mapView.setRegion(region, animated: true)
+        }
     }
 }
 
